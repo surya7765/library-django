9462ae8194118ad7246798c346ef7207b1db728e
Yugandhar Surya <suryayugandhar300@gmail.com>
2022-09-13 14:04:38 +0530

    added new files

--- books/urls.py
+++ books/urls.py
@@ -3,7 +3,4 @@ from books.views import BookListView
 
 urlpatterns = [
     path('', BookListView.as_view(), name="book-home"),
-    # path('user/<str:id>/', UserPostListView.as_view(), name="user-posts"),
-    # path('post/<int:pk>/', PostDetailView.as_view(), name="post-detail"),
-    # path('about/', views.about, name='blog-about'),
 ]
